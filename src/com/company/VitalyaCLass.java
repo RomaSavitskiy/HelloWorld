9d14277afd65e14b35f9f56c16b274551bc903cc
RomanSavitskiy <roma.savitskiy@inbox.ru>
2022-07-03 13:18:33 +0300

    Updated VitalyaClass

--- src/com/company/VitalyaCLass.java
+++ src/com/company/VitalyaCLass.java
@@ -4,5 +4,6 @@ public class VitalyaCLass {
 
     public static void VitalyaMessage(String[] args){
         System.out.println("Hello Vital");
+        System.out.println("Use Git Bash");
     }
 }
